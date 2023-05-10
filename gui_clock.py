9245dc0c96d428ba0dc4483a8cd28f512607b503
stephan <stephen.49251@gmail.com>
2023-05-10 18:17:21 +0200

    code updates

--- gui_clock.py
+++ gui_clock.py
@@ -15,22 +15,24 @@
 '''
 
 '''
-pip install customtkinter
-
-
+Dependencies: 
+    - customtkinter
+    - tkinter
+    - time
+    - filedialog
 '''
 
 import time
 import customtkinter as tk
 from tkinter import filedialog
+import os as os
 
 class Clock:
 
     def __init__(self):
         self.time = time.localtime() # the current time as a time object
-        # weekday as a string
         self.weekday = time.strftime('%A', self.time) # the weekday as a string: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
-        self.date = f'{self.time.tm_mday}-{self.time.tm_mon}-{self.time.tm_year}' # the date in the format: 01-05-2023
+        self.date = f'{self.time.tm_mday}.{self.time.tm_mon}.{self.time.tm_year}' # the date in the format: 01-05-2023
         self.current_time = f'{self.time.tm_hour}:{self.time.tm_min}:{self.time.tm_sec}' # the time in the format: 12:30:15
 
         # timer variables
@@ -38,6 +40,8 @@ class Clock:
         self.timer_start_time = None
         self.timer_end_time = None
         self.timer_duration = None
+        self.DEFAULT_TIMER_PATH = os.path.join(os.getcwd(), 'time.txt')
+        self.CHOSEN_TIMER_PATH = self.DEFAULT_TIMER_PATH
 
         # countdown variables
         self.countdown_running = False
@@ -56,9 +60,28 @@ class Clock:
         self.date_label.pack(pady=5, padx=10)
         
         # timer widgets
-        # timer button, will start the timer when clicked and change its text to 'Stop timer'. When clicked again, it will stop the timer and change its text back to 'Start timer'
-        self.timer_button = tk.CTkButton(frame, text='Start timer', command=self.toggle_timer) # timer button
-        self.timer_button.pack(pady=12, padx=10)
+        self.timer_frame = tk.CTkFrame(frame) # timer frame
+        self.timer_frame.pack(pady=5, padx=10) 
+
+        self.timer_button = tk.CTkButton(self.timer_frame, text='Start timer', command=self.toggle_timer) # timer button
+        self.timer_button.grid(row=1, column=0, padx=10, pady=5)
+
+        self.timer_path_entry = tk.CTkEntry(self.timer_frame, width=480, font=("", 11), placeholder_text=self.CHOSEN_TIMER_PATH) # timer path entry
+        self.timer_path_entry.bind('<KeyRelease>', lambda event: self.modify_path)
+        self.timer_path_entry.grid(row=0, column=0, padx=10, pady=5)
+
+        self.timer_path_chooser = tk.CTkButton(self.timer_frame, text='C', command=self.select_file, width=25) # timer path chooser button
+        self.timer_path_chooser.grid(row=0, column=1)
+        
+
+        # countdown widgets
+        countdown_frame = tk.CTkFrame(frame) # countdown frame
+        countdown_frame.pack(pady=5, padx=10)
+        self.countdown_min = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Min') # countdown minutes
+        # center it under the timer button
+        self.countdown_min.grid(row=0, column=0, padx=10, pady=5)
+        self.countdown_sec = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Sec') # countdown Seconds
+        self.countdown_sec.grid(row=0, column=1, padx=10, pady=5)
 
         self.update() # update the time every second
 
@@ -66,7 +89,7 @@ class Clock:
     def update(self):
         self.time = time.localtime()
         self.weekday = self.time.tm_wday
-        self.date = f'{self.time.tm_mday}-{self.time.tm_mon}-{self.time.tm_year}'
+        self.date = f'{self.time.tm_mday}.{self.time.tm_mon}.{self.time.tm_year}'
         self.current_time = f'{self.time.tm_hour}:{self.time.tm_min}:{self.time.tm_sec}'
         self.label.configure(text=str(self.current_time)) # update the time label
         root.after(1000, self.update) # update the time every second
@@ -78,30 +101,54 @@ class Clock:
             self.timer_running = True
             self.timer_start_time = time.localtime()
             print('timer started')
-            self.timer_button.configure(text="Stop timer")
+            self.timer_button.configure(text="Stop timer", fg_color='red', hover_color='#8b0000')
         else: 
             self.timer_running = False
             self.timer_end_time = time.localtime()
             self.timer_duration = time.mktime(self.timer_end_time) - time.mktime(self.timer_start_time)
-
-            def select_file():
-                file_path = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
-                return str(file_path)
             
-            path = select_file()
+            path = self.CHOSEN_TIMER_PATH
             self.write_timer(path)
             print('timer stopped')  
-            self.timer_button.configure(text="Start timer")     
+            self.timer_button.configure(text="Start timer", fg_color='blue', hover_color='#00008b') 
+
+    def select_file(self):
+            file_path = filedialog.askopenfilename(initialdir="/", title="Select file", filetypes=(("Text files", "*.txt"), ("All files", "*.*")))
+            self.CHOSEN_TIMER_PATH = str(file_path)
+            self.timer_path_entry.configure(placeholder_text=self.CHOSEN_TIMER_PATH)
+
+    def modify_path(self):
+        self.CHOSEN_TIMER_PATH = self.timer_path_entry.get()
+        self.timer_path_entry.configure(placeholder_text=self.CHOSEN_TIMER_PATH)
+
 
     def write_timer(self, path):
-        if self.timer_running == False:
-            with open(path, 'a') as f:
+        # if path exists, append to it. If not, create it, write the header and then append to it
+        if os.path.exists(path):
+             with open(path, 'a') as f:
                 f.write(f'{self.timer_start_time.tm_hour}:{self.timer_start_time.tm_min}:{self.timer_start_time.tm_sec} - {self.timer_end_time.tm_hour}:{self.timer_end_time.tm_min}:{self.timer_end_time.tm_sec} - {self.timer_duration}\n')
-            print('timer written to file')
-            return True
+                print('file exists')
+                print('timer written to file')
+                return True
         else:
-            print('timer still running')
-            return False
+            with open(path, 'w') as f:
+                f.write('Start time - End time - Duration\n')
+                f.write(f'{self.timer_start_time.tm_hour}:{self.timer_start_time.tm_min}:{self.timer_start_time.tm_sec} - {self.timer_end_time.tm_hour}:{self.timer_end_time.tm_min}:{self.timer_end_time.tm_sec} - {self.timer_duration}\n')
+                print('file created')
+                print('timer written to file')
+                return True
+
+
+        '''with open(path, 'a') as f:
+            f.write(f'{self.timer_start_time.tm_hour}:{self.timer_start_time.tm_min}:{self.timer_start_time.tm_sec} - {self.timer_end_time.tm_hour}:{self.timer_end_time.tm_min}:{self.timer_end_time.tm_sec} - {self.timer_duration}\n')
+        print('timer written to file')
+        return True'''
+        
+    # implementation of the countdown
+
+    #def toggle_countdown(self):
+    
+
 
 
 
@@ -112,7 +159,9 @@ tk.set_default_color_theme('dark-blue')
 
 # main window
 root = tk.CTk()
-root.geometry('500x300') 
+root.geometry('600x350') 
+root.title('Clock')
+root.resizable(True, True)
 
 # main frame
 frame = tk.CTkFrame(root)
