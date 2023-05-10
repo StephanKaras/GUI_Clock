1561e192f0c5877395c06ea3786689f71c1dd4e8
stephan <stephen.49251@gmail.com>
2023-05-11 00:44:09 +0200

    finalized version

--- gui_clock.py
+++ gui_clock.py
@@ -1,20 +1,5 @@
 '''
-▶ Write a graphical user interface (GUI) for a clock. 
-▶ The clock should graphically display the current time (hour, minute, seconds), day of week (e.g. Friday) and date (e.g. 01. Mai 2023). This display should be readable and nicely formated other than that, you may choose the display mode freely (e.g. write date as 2023-05-01).
-▶ The GUI contains a button that starts a timer (e.g. has a label Start timer or something similar). After the timer has started, the button changes its labeling (e.g. to Stop timer). When the timer is stopped, the start time, end time and duration are written to a file.
-▶ The timer file is created if nonexistent, otherwise new timings
-   are always added to the end of it.
 
-▶ There is an editable text field to define the location of the timer file. A button goes along with it that opens a file selection dialog and stores the resulting path in the editable field. The timer file is always defined by the editable field.
-▶ In addition, there are 2 text fields for setting a minute and seconds countdown. There is a Countdown button that starts the countdown. After the countdown, a message is presented to the user informing them that the countdown has ended. The countdown is updated live in the text fields.
-▶ Timer and countdown can be used simultaneously.
-
-▶ There are labels for the countdown text fields that mark them as minutes and seconds.
-▶ The timer button may not only change its text when started, but
-   can also change its color.
-'''
-
-'''
 Dependencies: 
     - customtkinter
     - tkinter
@@ -45,9 +30,9 @@ class Clock:
 
         # countdown variables
         self.countdown_running = False
-        self.countdown_start_time = None
-        self.countdown_end_time = None
-        self.countdown_duration = None
+        self.countdown_start_min = None
+        self.countdown_start_sec = None
+        self.repeating_every_second = None # the function that is called every second to update the countdown
 
         # Widgets
         self.label = tk.CTkLabel(frame, text=str(self.current_time), font=("", 40)) # time label
@@ -70,26 +55,36 @@ class Clock:
         self.timer_path_entry.bind('<KeyRelease>', lambda event: self.modify_path()) # bind the modify_path function to the entry, which updates the path variable when the user changes the path
         self.timer_path_entry.grid(row=0, column=0, padx=3, pady=5)
 
-        self.timer_path_chooser = tk.CTkButton(self.timer_frame, text='🔍', command=self.select_file, width=25) # timer path chooser button
+        self.timer_path_chooser = tk.CTkButton(self.timer_frame, text='📁', command=self.select_file, width=25) # timer path chooser button
         self.timer_path_chooser.grid(row=0, column=1, padx=0)
         
 
         # countdown widgets
         countdown_frame = tk.CTkFrame(frame) # countdown frame
         countdown_frame.pack(pady=5, padx=10)
-        self.countdown_min = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Min') # countdown minutes
-        self.countdown_min.grid(row=0, column=0, padx=5, pady=5)# center it under the timer button
+
+        self.countdown_min_entry = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Min') # countdown minutes
+        self.countdown_min_entry.grid(row=0, column=0, padx=5, pady=0)# center it under the timer button
+
+        self.countdown_min_label = tk.CTkLabel(countdown_frame, text="Min", font=("", 10)) # Minute label
+        self.countdown_min_label.grid(row=1, column=0, padx=0, pady=0)
 
         self.countdown_label = tk.CTkLabel(countdown_frame, text=':', font=("", 20)) # countdown label
         self.countdown_label.grid(row=0, column=1, padx=0, pady=5)
 
 
-        self.countdown_sec = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Sec') # countdown Seconds
-        self.countdown_sec.grid(row=0, column=2, padx=5, pady=5)
+        self.countdown_sec_entry = tk.CTkEntry(countdown_frame, width=50, font=("", 20), placeholder_text='Sec') # countdown Seconds
+        self.countdown_sec_entry.grid(row=0, column=2, padx=5, pady=0)
+
+        self.countdown_sec_label = tk.CTkLabel(countdown_frame, text="Sec", font=("", 10)) # Second label
+        self.countdown_sec_label.grid(row=1, column=2, padx=0, pady=0)
+
+        self.countdown_button = tk.CTkButton(countdown_frame, text='Start countdown', command=self.toggle_countdown) # countdown button
+        self.countdown_button.grid(row=3, column=0, columnspan=3, padx=0, pady=5)
 
         self.update() # update the time every second
 
-    # upadte the time every second
+    # update the time every second
     def update(self):
         self.time = time.localtime()
         self.weekday = self.time.tm_wday
@@ -145,12 +140,97 @@ class Clock:
                 print('file created')
                 print('timer written to file')
                 return True
+            
+    # implementation of the countdown toggle
+    def toggle_countdown(self):
+        if not self.countdown_running:
+            self.countdown_start_min = self.countdown_min_entry.get() # get the minutes from the entry field
+            self.countdown_start_sec = self.countdown_sec_entry.get() # get the seconds from the entry field
 
-        
-    # implementation of the countdown
+            # check input validity
+            if self.countdown_start_min == '' and self.countdown_start_sec == '':
+                #messagebox.showerror('Error', 'Please enter a valid time')
+                self.employ_error_message('Please enter a valid time')
+                print('Please enter a valid time')
+                return False
+            
+            try: # check if the input is a number and convert it to an integer if it is
+                if self.countdown_start_min and self.countdown_start_min.isdigit():
+                    self.countdown_start_min = int(self.countdown_start_min)
+                else: 
+                    self.countdown_start_min = 0
 
-    #def toggle_countdown(self):
+                if self.countdown_start_sec and self.countdown_start_sec.isdigit():
+                    self.countdown_start_sec = int(self.countdown_start_sec)
+                else:
+                    self.countdown_start_sec = 0
+
+            except ValueError:
+                self.employ_error_message('Please enter a valid time')
+                print('Please enter a valid time')
+                return False
+            
+            if self.countdown_start_min < 0 or self.countdown_start_sec < 0:
+                self.employ_error_message('Please enter a valid time')
+                print('Please enter a valid time')
+                return False
+            elif self.countdown_start_min == 0 and self.countdown_start_sec == 0:
+                self.employ_error_message('Please enter a valid time')
+                print('Please enter a valid time')
+                return False
+            else:
+                self.countdown_running = True
+                
+                # convert the minutes and seconds to integers
+                self.countdown_start_min = int(self.countdown_start_min)
+                self.countdown_start_sec = int(self.countdown_start_sec)
+
+                self.countdown_button.configure(text="Stop countdown", fg_color='red', hover_color='#8b0000') # change the button text and color
+                print('countdown started')
+                self.countdown()
+        else:
+            self.countdown_running = False
+            self.countdown_button.configure(text="Start countdown", fg_color='#1F538D', hover_color='#204B7C')
+
+             # Cancel the repeating event
+            if self.repeating_every_second is not None:
+                root.after_cancel(self.repeating_every_second)
+                self.repeating_every_second = None
+
+            print('countdown stopped')
+            return False
     
+    # implementation of the countdown functionality
+    def countdown(self):
+        if self.countdown_running: # while the countdown is running
+            if self.countdown_start_min == 0 and self.countdown_start_sec == 0: # if the minutes and seconds are 0, stop the countdown
+                self.countdown_running = False
+                self.countdown_button.configure(text="Start countdown", fg_color='#1F538D', hover_color='#204B7C')
+                #root.after_cancel(self.repeating_every_second) # cancel the repeating function
+                print('countdown stopped')
+                return False
+            elif self.countdown_start_sec == 0: # if the seconds are 0, decrement the minutes and set the seconds to 59
+                self.countdown_start_min -= 1 
+                self.countdown_start_sec = 59
+            else:
+                self.countdown_start_sec -= 1
+
+            self.countdown_min_entry.delete(0, tk.END)  # delete the old value
+            self.countdown_min_entry.insert(0, self.countdown_start_min)  # insert the new value
+            self.countdown_sec_entry.delete(0, tk.END)  # delete the old value
+            self.countdown_sec_entry.insert(0, self.countdown_start_sec)  # insert the new value
+        
+        self.repeating_every_second = root.after(1000, self.countdown) # call the countdown function every second
+
+    # creates an error message window
+    def employ_error_message(self, message):
+        error_message = tk.CTkToplevel() # create a new window
+        error_message.geometry('400x200')
+        error_message.title('Error')
+        error_message.attributes('-topmost', True)  # show the error message in front of the main window
+        error_message.resizable(False, False)
+        error_message_label = tk.CTkLabel(error_message, text=":( " + message, font=('', 12), fg_color='#8b0000')
+        error_message_label.pack(pady=10, padx=10, fill=tk.BOTH, expand=True)
 
 
 
@@ -162,7 +242,7 @@ tk.set_default_color_theme('dark-blue')
 
 # main window
 root = tk.CTk()
-root.geometry('600x350') 
+root.geometry('600x400') 
 root.title('Clock')
 root.resizable(True, True)
 
